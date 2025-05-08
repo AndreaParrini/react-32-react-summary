7411a422af416d9258b6aedce0afa5244352ad7d
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-05-08 11:02:22 +0200

    add NewPost components and add style, import in PostsList component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 
 import classes from "./PostsList.module.css"
 
 function PostsList(){
     return (
-        <ul className={classes.posts}>
-            <Post author="Andrea" body="React.js is awsome!"/>
-            <Post author="Maximilian" body="Check out the full course!"/>
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Andrea" body="React.js is awsome!"/>
+                <Post author="Maximilian" body="Check out the full course!"/>
+            </ul>
+        </>
     )
 }
 
