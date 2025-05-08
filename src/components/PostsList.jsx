c7a6a9fd0102e913e6e54f540935d5b0b9632ac2
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-05-08 11:19:01 +0200

    lifting up state and add a new stato to manage name of author

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+
 import NewPost from "./NewPost";
 import Post from "./Post";
 
 import classes from "./PostsList.module.css"
 
 function PostsList(){
+    const [enteredBody, setEnteredBody] = useState("React.js is awsome!");
+    const [enteredAuthor, setEnteredAuthor] = useState("Andrea");
+
+
+    function changeBodyHandler(event){
+        setEnteredBody(event.target.value)
+    }
+
+     function changeAuthorHandler(event){
+        setEnteredAuthor(event.target.value)
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
             <ul className={classes.posts}>
-                <Post author="Andrea" body="React.js is awsome!"/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Maximilian" body="Check out the full course!"/>
             </ul>
         </>
