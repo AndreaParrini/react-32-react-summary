daf6d834c39b50bd3d8b5589bf49833ccb8f931b
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-05-08 11:33:43 +0200

    import Modal component and manage state to view content conditionally

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,23 +4,34 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 
 import classes from "./PostsList.module.css"
+import Modal from './Modal';
 
 function PostsList(){
     const [enteredBody, setEnteredBody] = useState("React.js is awsome!");
     const [enteredAuthor, setEnteredAuthor] = useState("Andrea");
+    const [modalIsVisible, setModaliIsVisible] = useState(true)
 
 
     function changeBodyHandler(event){
         setEnteredBody(event.target.value)
     }
 
-     function changeAuthorHandler(event){
+    function changeAuthorHandler(event){
         setEnteredAuthor(event.target.value)
     }
 
+    function hideModalContent(){
+        setModaliIsVisible(false) 
+    }
+    
+
     return (
-        <>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
+        <>  
+            {modalIsVisible && (
+                <Modal onClose={hideModalContent}>
+                    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
+                </Modal>
+            )}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Maximilian" body="Check out the full course!"/>
